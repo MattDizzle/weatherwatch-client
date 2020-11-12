7f7c40248950a3e2c1582ae41ebe030afb138cc2
Matthew Rougely <matthewrougelysr@gmail.com>
2020-11-12 03:55:52 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Weather Watch</h1>
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
