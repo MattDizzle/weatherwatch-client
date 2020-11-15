a9b96f8f3fd187d0dd6f5d7d42d3d4ba5fdae7e2
Matthew Rougely <matthewrougelysr@gmail.com>
2020-11-15 06:15:01 -0800

    Update LoadingScreen.js

--- src/LoadingScreen/LoadingScreen.js
+++ src/LoadingScreen/LoadingScreen.js
@@ -3,9 +3,9 @@ import './LoadingScreen.css'
 
 export const LoadingScreen = () => {
     return(
-        <div className='loading-box'>
-            <span>Loading</span>
-        <img src={LoadingRing} alt='loading-ring' className='ring' />
+        <div className='loading-box animate__animated animate__zoomIn'>
+            <span className='animate__animated animate__flash animate__infinite animate__slow'>Loading</span>
+            <img src={LoadingRing} alt='loading-ring' className='ring animate__animated animate__zoomIn ' />
         </div>
     )
 }
